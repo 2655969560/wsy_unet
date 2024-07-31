cd8092022330fea1e9dd4e82f03e81779edd271e
2655969560 <2655969560@qq.com>
2024-07-31 12:28:55 +0800

    0731 linux path

--- train.py
+++ train.py
@@ -22,8 +22,8 @@ from unet import UNet
 from utils.data_loading import BasicDataset, CarvanaDataset
 from utils.dice_score import dice_loss
 
-dir_img = Path('../data/images')
-dir_mask = Path('../data/masks')
+dir_img = Path('/media/bmo/TOSHIBA_2T_HD/Data/20240731/data_sum/20240729/RGB2')
+dir_mask = Path('/media/bmo/TOSHIBA_2T_HD/Data/20240731/data_sum/20240729/masks2')
 dir_checkpoint = Path('./checkpoints/')
 
 
@@ -187,7 +187,7 @@ def train_model(
 
 def get_args():
     parser = argparse.ArgumentParser(description='Train the UNet on images and target images')
-    parser.add_argument('--epochs', '-e', metavar='E', type=int, default=2, help='Number of epochs')
+    parser.add_argument('--epochs', '-e', metavar='E', type=int, default=50, help='Number of epochs')
     parser.add_argument('--batch-size', '-b', dest='batch_size', metavar='B', type=int, default=4, help='Batch size')
     parser.add_argument('--learning-rate', '-l', metavar='LR', type=float, default=0.00001,
                         help='Learning rate', dest='lr')
@@ -211,7 +211,7 @@ if __name__=='__main__':
     # Change here to adapt to your data
     # n_channels=3 for RGB images
     # n_classes is the number of probabilities you want to get per pixel
-    model = UNet(n_channels=1, n_classes=args.classes, bilinear=args.bilinear)
+    model = UNet(n_channels=3, n_classes=args.classes, bilinear=args.bilinear)
     model = model.to(memory_format=torch.channels_last)
 
     logging.info(f'Network:\n'
